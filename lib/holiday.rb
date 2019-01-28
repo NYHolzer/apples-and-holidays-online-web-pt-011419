b088f9f65eb86c709445f643124375c2e1433549
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-28 00:00:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.map do |season, holidays|
     holiday_hash[season].select do |holiday_name, supplies|
-      if supplies.includes?("BBQ")
+      if supplies.include?("BBQ")
         holiday_name
       end
     end
